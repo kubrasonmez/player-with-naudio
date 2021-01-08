e26f946507b556d1415b614bf0b16462762eb59a
Kübra Sönmez <ksonmez@cornea.aero>
2021-01-08 08:28:30 +0300

    Implemented a player with naudio.

---
+++ NAudioPlayer/Player.cs
@@ -0,0 +1,54 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NAudioPlayer
+{
+    public class Player
+    {
+        public WaveOutEvent output;
+        public string Path { get; }
+        public Player(string path)
+        {
+            Path = path;
+            output = new WaveOutEvent();
+        }
+        public void PlayWav()
+        {
+            if (output.PlaybackState == PlaybackState.Stopped)
+            {
+                Play();
+            }
+            else if (output.PlaybackState == PlaybackState.Paused)
+            {
+                Resume();
+            }
+            else
+            {
+                return;
+            }
+        }
+        private void Play()
+        {
+            FileStream wav_file_stream = File.OpenRead(Path);
+
+            // Reset the stream to the beginning
+            wav_file_stream.Seek(0, SeekOrigin.Begin);
+
+            WaveStream ws_raw = new WaveFileReader(wav_file_stream);
+            ws_raw = WaveFormatConversionStream.CreatePcmStream(ws_raw);
+            output.Init(ws_raw);
+            output.Play();
+        }
+
+        private void Resume() => output.Play();
+        public void Pause() => output.Pause();
+        public void Stop() => output.Stop();
+        public PlaybackState GetStatus() => output.PlaybackState;
+
+
+
+    }
+}
---
+++ NAudioPlayer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using NAudio.Wave;
+namespace NAudioPlayer
+{
+    class Program
+    {
+        private static string wav_file_path = @"D:\sound.wav";
+        private static string wav_file_path2 = @"D:\sound2.wav";
+
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(wav_file_path);
+            Player player2 = new Player(wav_file_path2);
+            player1.output.PlaybackStopped += new EventHandler<StoppedEventArgs>(audioOutput_Playback1Stopped);
+            player2.output.PlaybackStopped += new EventHandler<StoppedEventArgs>(audioOutput_Playback2Stopped);
+            player1.PlayWav();
+            Thread.Sleep(3000);
+            player2.PlayWav();
+            Thread.Sleep(7000);
+            player2.Pause();
+            Thread.Sleep(3000);
+            Console.WriteLine($"Player1 : {player1.GetStatus()}");
+            player1.Stop();
+            Console.WriteLine($"Player 2 : {player2.GetStatus()}");
+            player2.PlayWav();
+            Console.WriteLine($"Player 1 : {player1.GetStatus()}");
+            Console.WriteLine($"Player 2 : {player2.GetStatus()}");
+            Console.Write("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static void audioOutput_Playback1Stopped(object sender, StoppedEventArgs e)
+        {
+            Console.WriteLine($"Player1 durdu.");
+        }
+        private static void audioOutput_Playback2Stopped(object sender, StoppedEventArgs e)
+        {
+            Console.WriteLine($"Player1 durdu.");
+        }
+    }
+}
